a4a2e3fb408128483d0be2056e87619f07a2e48b
Lezalo <141935585+Lezalo@users.noreply.github.com>
2026-04-08 22:49:06 -0500

    Update Persona.java

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -4,4 +4,23 @@ public class Persona {
 	// Atributos
     private String nombre;
     private int edad;
+
+	// Constructor
+    public CASO2 (String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+	// Impresion 
+	public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre + " | Edad: " + edad);
+
+    }
+    public static void main(String[] args) {
+        Persona p1 = new Persona ("Rafael", 25);
+        Persona p2 = new Persona ("Jairo", 30);
+
+        p1.mostrarDatos();
+        p2.mostrarDatos();
+}
 }
